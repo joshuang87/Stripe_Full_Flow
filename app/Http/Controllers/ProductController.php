fe527515983aaf60c5067b019db77532a867312e
joshuang87 <joshuang43@gmail.com>
2023-07-29 21:11:48 +0800

    completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProductController extends Controller
 {
@@ -68,10 +69,25 @@ class ProductController extends Controller
         {
             $session = $stripe->checkout->sessions->retrieve($checkout_session_id);
             // dd($session);
-            $customer = $stripe->customers->retrieve($session->customer_details);
+            $customer = $session->customer_details;
             // $customerName = $customer['name'];
             echo "<h1>Thanks for your order, $customer->name!</h1>";
             http_response_code(200);
+
+            $order = Order::where('session_id',$session->id)->first();
+            if(!$order)
+            {
+                throw new NotFoundHttpException();
+            }
+
+            if($order && $order->status === 'unpaid')
+                {
+                    $order->status = 'paid';
+                    $order->save();
+                    //  send email
+                }
+
+            return view('product.checkout-success',compact('customer'));
         }
         catch(Error $e)
         {
@@ -79,11 +95,67 @@ class ProductController extends Controller
             echo json_encode(['error' => $e->getMessage()]);
         }
 
-        return view('product.checkout-success',compact('customer'));
     }
 
     public function cancel()
     {
 
+    }
+
+    public function webhook()
+    {
+        // The library needs to be configured with your account's secret key.
+        // Ensure the key is kept out of any version control system you might be using.
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
+
+        // This is your Stripe CLI webhook secret for testing your endpoint locally.
+        $endpoint_secret = env('STRIPE_WEBHOOK_SECRET');
+
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try
+        {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sig_header, $endpoint_secret
+            );
+        }
+        catch(\UnexpectedValueException $e)
+        {
+            // Invalid payload
+            // http_response_code(400);
+            // exit();
+            return response($e,400);
+        }
+        catch(\Stripe\Exception\SignatureVerificationException $e)
+        {
+            // Invalid signature
+            // http_response_code(400);
+            // exit();
+            return response($e,400);
+        }
+
+        // Handle the event
+        switch ($event->type)
+        {
+            case 'checkout.session.completed':
+                $session = $event->data->object;
+                $sessionId = $session->id;
+
+                $order = Order::where('session_id',$session->id)->first();
+                if($order && $order->status === 'unpaid')
+                {
+                    $order->status = 'paid';
+                    $order->save();
+                    //  send email
+                }
+            // ... handle other event types
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
+
+        // http_response_code(200);
+        return response('',200);
     }
 }
